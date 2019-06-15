73d9193cdb27f4dc48c0a2863e751236ddc0ac7a
Koichi Taura <taura.koichi@gmail.com>
2019-06-16 07:28:49 +0900

    bugfix reading config file correctly

--- config/config_manager.py
+++ config/config_manager.py
@@ -8,7 +8,8 @@ class ConfigManager:
     def __init__(self, file_path):
         current_path = os.getcwd()
         self.config = configparser.ConfigParser()
-        self.config.read(current_path + file_path)
+        self.config.read(current_path + "/" + file_path)
+        print("read config file with:" + current_path + file_path)
 
     @abstractmethod
     def get_config_obj(self):
