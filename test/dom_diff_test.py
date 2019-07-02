20c7ba9b816ff408ba025cd5f6ad5b3a3438b938
Koichi Taura <taura.koichi@gmail.com>
2019-07-03 00:29:44 +0900

    add test to confirm that the tool only show dom that has difference

--- test/dom_diff_test.py
+++ test/dom_diff_test.py
@@ -14,3 +14,9 @@ class DomDiffTest(TestCase):
         html1 = "<html><body><h1>Hello World</h1></body></html>"
         html2 = "<html><body><h1>Hello Hello</h1></body></html>"
         self.assertEqual("html > body > h1\n+ Hello World\n- Hello Hello\n", dom_diff.compare(html1, html2))
+
+    def test_compare_h2_diff(self):
+        dom_diff = DomDiff()
+        html1 = "<html><body><h1>Hello World</h1><div><h2>Hoge Hoge</h2></div></body></html>"
+        html2 = "<html><body><h1>Hello World</h1><div><h2>Fuga Fuga</h2></div></body></html>"
+        self.assertEqual("html > body > div > h2\n+ Hoge Hoge\n- Fuga Fuga\n", dom_diff.compare(html1, html2))
