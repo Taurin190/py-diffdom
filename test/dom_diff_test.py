669b72d13b69c40ffe6cc5e211c1985bdddaaf82
Koichi Taura <taura.koichi@gmail.com>
2019-07-02 23:48:24 +0900

    add red pattern of compare test though I though the test would pass

--- test/dom_diff_test.py
+++ test/dom_diff_test.py
@@ -3,5 +3,8 @@ from domain.diff.dom_diff import DomDiff
 
 
 class DomDiffTest(TestCase):
-    def test_失敗するテスト実施(self):
-        self.assertEqual("", "失敗")
+    def test_compare_no_diff(self):
+        dom_diff = DomDiff()
+        html1 = "<html><body><h1>Hello World</h1></body></html>"
+        html2 = "<html><body><h1>Hello World</h1></body></html>"
+        self.assertEqual("", dom_diff.compare(html1, html2))
