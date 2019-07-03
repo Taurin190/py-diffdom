7fe6c9749ebd1422aa060a1069ee80b7c293d60f
Koichi Taura <taura.koichi@gmail.com>
2019-07-03 23:10:54 +0900

    refactor test preparing dom diff class

--- test/dom_diff_test.py
+++ test/dom_diff_test.py
@@ -3,20 +3,20 @@ from domain.diff.dom_diff import DomDiff
 
 
 class DomDiffTest(TestCase):
+    def setUp(self):
+        self.dom_diff = DomDiff()
+
     def test_compare_no_diff(self):
-        dom_diff = DomDiff()
         html1 = "<html><body><h1>Hello World</h1></body></html>"
         html2 = "<html><body><h1>Hello World</h1></body></html>"
-        self.assertEqual("", dom_diff.compare(html1, html2))
+        self.assertEqual("", self.dom_diff.compare(html1, html2))
 
     def test_compare_h1_diff(self):
-        dom_diff = DomDiff()
         html1 = "<html><body><h1>Hello World</h1></body></html>"
         html2 = "<html><body><h1>Hello Hello</h1></body></html>"
-        self.assertEqual("html > body > h1\n+ Hello World\n- Hello Hello\n", dom_diff.compare(html1, html2))
+        self.assertEqual("html > body > h1\n+ Hello World\n- Hello Hello\n", self.dom_diff.compare(html1, html2))
 
     def test_compare_h2_diff(self):
-        dom_diff = DomDiff()
         html1 = "<html><body><h1>Hello World</h1><div><h2>Hoge Hoge</h2></div></body></html>"
         html2 = "<html><body><h1>Hello World</h1><div><h2>Fuga Fuga</h2></div></body></html>"
-        self.assertEqual("html > body > div > h2\n+ Hoge Hoge\n- Fuga Fuga\n", dom_diff.compare(html1, html2))
+        self.assertEqual("html > body > div > h2\n+ Hoge Hoge\n- Fuga Fuga\n", self.dom_diff.compare(html1, html2))
